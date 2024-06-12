9cb594189827c7b76f3ead778ad8885a99f3c82d
The 0x <0xvmx@proton.me>
2024-06-11 21:39:25 -0500

    first lab

--- cloud-labs/azure/az-104/resource-groups/README.md
+++ cloud-labs/azure/az-104/resource-groups/README.md
@@ -4,3 +4,13 @@ In this lab, you will read and learn about Azure Resource Groups.
 Reference Documentation:
 - [Azure Resource Manager Overview](https://docs.microsoft.com/en-us/azure/azure-resource-manager/management/overview)
 - [The 0x - AZ-104 Guide](https://publish.obsidian.md/ysac/Azure/AZ-104/AZ-104)
+
+---
+## How to deploy Azure Resource Groups using Pulumi
+In this lab, deploy the Azure Resource Group using Pulumi defined in `main.go` file. Login to the Azure portal and verify the deployment of the resource group and the storage account. Notice how the storage account is tied to the resource group called `the0xResourceGroup`.
+
+## Goal
+The goal is to see how the Storage Account is tied to the Resource Group and that various resources objects are grouped together in a logical manner.
+
+## Cleanup
+After you have verified the deployment, remove the resources by running `pulumi destroy` and then `pulumi stack rm dev` to remove the stack.
---
+++ cloud-labs/azure/az-104/resource-groups/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"github.com/pulumi/pulumi-azure-native-sdk/resources/v2"
+	"github.com/pulumi/pulumi-azure-native-sdk/storage/v2"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		resourceGroup, err := resources.NewResourceGroup(ctx, "the0xResourceGroup", nil)
+		if err != nil {
+			return err
+		}
+
+		account, err := storage.NewStorageAccount(ctx, "the0xsa", &storage.StorageAccountArgs{
+			ResourceGroupName: resourceGroup.Name,
+			Sku: &storage.SkuArgs{
+				Name: pulumi.String("Standard_LRS"),
+			},
+			Kind: pulumi.String("StorageV2"),
+		})
+		if err != nil {
+			return err
+		}
+
+		ctx.Export("primaryStorageKey", pulumi.All(resourceGroup.Name, account.Name).ApplyT(
+			func(args []interface{}) (string, error) {
+				resourceGroupName := args[0].(string)
+				accountName := args[1].(string)
+				accountKeys, err := storage.ListStorageAccountKeys(ctx, &storage.ListStorageAccountKeysArgs{
+					ResourceGroupName: resourceGroupName,
+					AccountName:       accountName,
+				})
+				if err != nil {
+					return "", err
+				}
+
+				return accountKeys.Keys[0].Value, nil
+			},
+		))
+
+		return nil
+	})
+}
