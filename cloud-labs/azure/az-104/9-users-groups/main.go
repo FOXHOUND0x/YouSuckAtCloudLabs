9cf29ddc5a38a48dc83555077761e825e094154d
The 0x <0xvmx@proton.me>
2024-06-28 15:16:29 -0500

    mta and users-groups

--- cloud-labs/azure/az-104/9-users-groups/main.go
+++ cloud-labs/azure/az-104/9-users-groups/main.go
@@ -5,6 +5,29 @@ import (
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
+func errCheck(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func createGroup(ctx *pulumi.Context, name string, ownerObjectId pulumi.StringInput) (*azuread.Group, error) {
+	return azuread.NewGroup(ctx, name, &azuread.GroupArgs{
+		DisplayName:     pulumi.String(name),
+		MailNickname:    pulumi.String(name),
+		SecurityEnabled: pulumi.Bool(true),
+		Owners:          pulumi.StringArray{ownerObjectId},
+	})
+}
+
+func addGroupMember(ctx *pulumi.Context, groupName string, groupId pulumi.IDOutput, memberId pulumi.IDOutput) error {
+	_, err := azuread.NewGroupMember(ctx, groupName, &azuread.GroupMemberArgs{
+		GroupObjectId:  groupId.ToStringOutput(),
+		MemberObjectId: memberId.ToStringOutput(),
+	})
+	return err
+}
+
 func createUser(ctx *pulumi.Context, name, displayName, mailNickname, userPrincipalName, password string) (*azuread.User, error) {
 	return azuread.NewUser(ctx, name, &azuread.UserArgs{
 		DisplayName:         pulumi.String(displayName),
@@ -28,26 +51,55 @@ func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		const password = "P@ssw0rd"
 
-		ericW, err := createUser(ctx, "ericW", "Eric W", "ericw", "ericW@ysacthe0x.onmicrosoft.com", password)
-		if err != nil {
-			return err
+		current, err := azuread.GetClientConfig(ctx, nil, nil)
+		errCheck(err)
+
+		users := []struct {
+			name          string
+			displayName   string
+			mailNickname  string
+			userPrincipal string
+			roleName      string
+			roleID        string
+		}{
+			{"ericW", "Eric W", "ericw", "ericW@ysacthe0x.onmicrosoft.com", "GlobalAdminRole", "62e90394-69f5-4237-9190-012177145e10"},
+			{"issacQ", "Issac Q", "issacq", "issac@ysacthe0x.onmicrosoft.com", "IntuneAdmin", "3a2c62db-5318-420d-8d74-23affee5d9d5"},
+			{"JamieK", "Jamie K", "jamiek", "jamie@ysacthe0x.onmicrosoft.com", "appDevelop", "cf1c38e5-3621-4004-a7cb-879624dced7c"},
 		}
 
-		jamie, err := createUser(ctx, "JamieK", "Jamie K", "jamiek", "jamie@ysacthe0x.onmicrosoft.com", password)
-		if err != nil {
-			return err
+		var createdUsers []*azuread.User
+
+		for _, user := range users {
+			createdUser, err := createUser(ctx, user.name, user.displayName, user.mailNickname, user.userPrincipal, password)
+			errCheck(err)
+			createdUsers = append(createdUsers, createdUser)
+
+			_, err = createRoleAssignment(ctx, user.roleName, createdUser.ID(), user.roleID)
+			errCheck(err)
 		}
 
-		if _, err := createRoleAssignment(ctx, "globalAdmin", ericW.ID(), "62e90394-69f5-4237-9190-012177145e10"); err != nil {
-			return err
-		}
+		devGroup, err := createGroup(ctx, "devGroup", pulumi.String(current.ObjectId))
+		errCheck(err)
 
-		if _, err := createRoleAssignment(ctx, "appDevelop", ericW.ID(), "cf1c38e5-3621-4004-a7cb-879624dced7c"); err != nil {
-			return err
-		}
+		err = addGroupMember(ctx, "devGroup", devGroup.ID(), createdUsers[0].ID())
+		errCheck(err)
 
-		ctx.Export("userPrincipalName", ericW.UserPrincipalName)
-		ctx.Export("objectId", jamie.ObjectId)
+		adminUnit, err := azuread.NewAdministrativeUnit(ctx, "US-Admins", &azuread.AdministrativeUnitArgs{
+			DisplayName:             pulumi.String("Admins"),
+			Description:             pulumi.String("Administrative unit for global admins"),
+			HiddenMembershipEnabled: pulumi.Bool(false),
+			Members: pulumi.StringArray{
+				createdUsers[0].ID(),
+				createdUsers[1].ID(),
+			},
+			PreventDuplicateNames: pulumi.Bool(true),
+		})
+		errCheck(err)
+
+		ctx.Export("AdminUnit", adminUnit.ID())
+		ctx.Export("ericW", createdUsers[0].ID())
+		ctx.Export("jamie", createdUsers[2].ID())
+		ctx.Export("devGroup", devGroup.ID())
 
 		return nil
 	})
