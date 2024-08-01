f277b5a19c4bd14ba1f72c7fcca501dc4badef91
The 0x <0xvmx@proton.me>
2024-08-01 17:07:05 -0500

    organziational-fix

--- the0x-labs/aws/ragnar/infra/output.tf
+++
@@ -1,3 +0,0 @@
-output "api_url" {
-    value = aws_api_gateway_deployment.ragnar_deployment.invoke_url
-}
---
+++ the0x-labs/cloud-providers/aws/ragnar/infra/output.tf
@@ -0,0 +1,3 @@
+# output "api_url" {
+#     value = aws_api_gateway_deployment.ragnar_deployment.invoke_url
+# }
