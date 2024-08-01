ccc020f9f285be46260cf6eecdb5b4c52602f6f8
The 0x <0xvmx@proton.me>
2024-08-01 15:39:51 -0500

    policy updates

--- the0x-labs/cloud-providers/aws/services/identity-center/assignments/0xSec-team/group-assignments.tf
+++ the0x-labs/cloud-providers/aws/services/identity-center/assignments/0xSec-team/group-assignments.tf
@@ -1,16 +1,16 @@
 locals {
-  ai-team = [
+  0xSec-Team = [
   ]
 }
 
 resource "aws_identitystore_group" "ai_group" {
-  display_name      = "AI-Team"
-  description       = "Members of the AI team who will be using sagemaker and other AI services"
+  display_name      = "0xSec-Team"
+  description       = "the 0xsec dev team"
   identity_store_id = var.id_store_id
 }
 
 resource "aws_identitystore_group_membership" "ai_group_membership" {
-  for_each = { for user in local.ai-team : user.username => user }
+  for_each = { for user in local.0xSec-Team : user.username => user }
   identity_store_id = var.id_store_id
   group_id = aws_identitystore_group.ai_group.group_id
   member_id = each.value.userId
