898841ea9d6c4fb05753c52c98a956c0a3ddebef
The 0x <0xvmx@proton.me>
2024-08-02 19:41:10 -0500

    remove pipelines

--- the0x-labs/project-stacks/ragnar/infra/iam.tf
+++ the0x-labs/project-stacks/ragnar/infra/iam.tf
@@ -6,7 +6,7 @@ resource "aws_iam_role" "ragnar_lambda_role" {
             {
                 Action = "sts:AssumeRole"
                 Effect = "Allow"
-                Pincipal = {
+                Principal = {
                     Service = "lambda.amazonaws.com"
                 }
             }
@@ -14,38 +14,29 @@ resource "aws_iam_role" "ragnar_lambda_role" {
     })
 }
 
-# resource "aws_iam_role_policy" "ragnar_iam_policy" {
-#     name = "ragnar-lambda-policy"
-#     role = aws_iam_role.ragnar_lambda_role.id
-#     policy = jsonencode({
-#         Version = "2012-10-17",
-#         Statement = [
-#             {
-#                 Action = [
-#                     "dynamodb:GetItem",
-#                     "dynamodb:PutItem",
-#                     "dynamodb:UpdateItem",
-#                     "dynamodb:DeleteItem"
-#                 ]
-#                 Resource = aws_dynamodb_table.ragnar_table.arn
-#             },
-#             {
-#                 Effect = "Allow"
-#                 Action = [
-#                     "logs:CreateLogGroup",
-#                     "logs:CreateLogStream",
-#                     "logs:PutLogEvents"
-#                 ]
-#                 resource = "arn:aws:logs:*:*:*"
-#             }
-#         ]
-#     })
-# }
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+    role = aws_iam_role.ragnar_lambda_role.name
+    policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
 
-# resource "aws_lambda_permission" "ragnar_lambda_permission" {
-#     statement_id = "AllowExecutionFromAPIGateway"
-#     action = "lambda:InvokeFunction"
-#     function_name = aws_lambda_function.ragnar_lambda.function_name
-#     principal = "apigateway.amazonaws.com"
-#     source_arn = "${aws_api_gateway_rest_api.ragnar_api.execution_arn}/*/*"
-# }
+resource "aws_iam_role_policy" "ragnar_lambda_policy" {
+    name = "ragnar-lambda-policy"
+    role = aws_iam_role.ragnar_lambda_role.name
+    policy = jsonencode({
+        Version = "2012-10-17",
+        Statement = [
+            {
+                Action = [
+                    "s3:GetObject",
+                    "s3:PutObject",
+                    "s3:ListBucket"
+                ]
+                Effect = "Allow"
+                Resource = [
+                    "${aws_s3_bucket.ragnar_bucket.arn}",
+                    "${aws_s3_bucket.ragnar_bucket.arn}/*"
+                ]
+            }
+        ]
+    })
+}
--- the0x-labs/project-stacks/ragnar/infra/output.tf
+++ the0x-labs/project-stacks/ragnar/infra/output.tf
@@ -1,3 +1,7 @@
-# output "api_url" {
-#     value = aws_api_gateway_deployment.ragnar_deployment.invoke_url
-# }
+output "acm_cert_validation_records" {
+  value = aws_acm_certificate.ragnar_cert.domain_validation_options
+}
+
+output "api_gateway_domain" {
+    value = aws_apigatewayv2_domain_name.ragnar_domain.domain_name_configuration[0].target_domain_name
+}
